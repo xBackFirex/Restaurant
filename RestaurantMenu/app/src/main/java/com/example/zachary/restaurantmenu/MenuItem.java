dea3f8674bc010eabc26dfd9f563ef4e15d2db26
Zach Dailey <dailey.zachary1@gmail.com>
2016-03-09 20:37:05 -0500

    Pre-Spinner

---
+++ RestaurantMenu/app/src/main/java/com/example/zachary/restaurantmenu/MenuItem.java
@@ -0,0 +1,77 @@
+package com.example.zachary.restaurantmenu;
+
+/**
+ * Created by Zachary on 3/9/2016.
+ * Project: RestaurantMenu
+ */
+public class MenuItem
+{
+	private int _prodID;
+	private String _prodCat;
+	private String _prodName;
+	private double _prodPrice;
+	private String _prodDesc;
+
+	public MenuItem()
+	{
+	}
+
+	public MenuItem(int _prodID, String _prodCat, String _prodName, double _prodPrice, String _prodDesc)
+	{
+		this._prodID = _prodID;
+		this._prodCat = _prodCat;
+		this._prodName = _prodName;
+		this._prodPrice = _prodPrice;
+		this._prodDesc = _prodDesc;
+	}
+
+	public int get_prodID()
+	{
+		return _prodID;
+	}
+
+	public void set_prodID(int _prodID)
+	{
+		this._prodID = _prodID;
+	}
+
+	public String get_prodCat()
+	{
+		return _prodCat;
+	}
+
+	public void set_prodCat(String _prodCat)
+	{
+		this._prodCat = _prodCat;
+	}
+
+	public String get_prodName()
+	{
+		return _prodName;
+	}
+
+	public void set_prodName(String _prodName)
+	{
+		this._prodName = _prodName;
+	}
+
+	public double get_prodPrice()
+	{
+		return _prodPrice;
+	}
+
+	public void set_prodPrice(double _prodPrice)
+	{
+		this._prodPrice = _prodPrice;
+	}
+
+	public String get_prodDesc()
+	{
+		return _prodDesc;
+	}
+
+	public void set_prodDesc(String _prodDesc)
+	{
+		this._prodDesc = _prodDesc;
+	}
+}
---
+++ RestaurantMenu/app/src/main/java/com/example/zachary/restaurantmenu/RestDBHandler.java
@@ -0,0 +1,8 @@
+package com.example.zachary.restaurantmenu;
+
+/**
+ * Created by Zachary on 3/9/2016.
+ */
+public class RestDBHandler
+{
+}
